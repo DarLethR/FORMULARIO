40cafb76447ab997a0d0584298da8a99fa3d7485
DarLethR <146796912+DarLethR@users.noreply.github.com>
2024-04-03 23:07:38 -0600

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('registrationForm');
-    
-    form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe automáticamente
-    
-    // Validar los campos del formulario
-    const nombre = document.getElementById('nombre').value;
-    const empresa = document.getElementById('empresa').value;
-    const interes = document.getElementById('interes').value;
-    
-    if (!nombre || !email || !empresa || !interes) {
-    alert('Por favor, complete todos los campos del formulario.');
-    return; // Evita que el formulario se envíe si falta algún campo
-    }
-    
-//Envia el formulario
-    form.submit();
+function iniciarMap(){
+    var coord = {lat:-34.5956145 ,lng: -58.4431949};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
     });
-   });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
